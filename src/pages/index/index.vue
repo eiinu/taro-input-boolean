b306eab77de8397879c9dd4bcf1159388f5d8877
Eiinu <eiinu@qq.com>
2024-06-20 17:12:45 +0800

    init commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <view class="index">
+    <input placeholder="可正常输入"/>
+    <input :disabled="false" placeholder="被禁用"/>
+    <input :disabled="true" placeholder="被禁用"/>
+  </view>
+</template>
